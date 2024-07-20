a9f4e8639c5f234283e1399b428b3401cc797b48
9OAT <oathaha30@gmail.com>
2024-07-21 01:01:21 +0700

    ert

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,38 @@
 <template>
-  <div class="bg-slate-500 h-20">
-    <div class="pt-6 pl-12 text-white">
-      <a href="/ff" target="_self">ชั้นหนังสือ</a>
-      <a class="pl-12" href="/howto" target="_self">วิธีใช้งาน</a>
+  <div>
+    <div class="w-1/6 h-[100%] bg-gray-700">
+      <div class="text-white p-9 font-bold text-xl text-center">todofast</div>
+      <div class="text-lg flex flex-col gap-4 pl-8">
+        <div class="text-white font-semibold">My List</div>
+        <div class="TodoFolder">
+          Life
+        </div>
+        <div class="TodoFolder">
+          Work
+        </div>
+        <div class="AddNew pl-2">
+          + New List
+        </div>
+      </div>
     </div>
-    
   </div>
-  <Header />
-  <suggest />
-  <mm />
-  <Navbar />
-
 </template>
+<style scoped>
+.TodoFolder {
+  cursor: pointer;
+  padding-left: 8px;
+  padding-right: 8px;
+}
+
+.TodoFolder:hover {
+  background-color: yellow;
+  border-radius: 25px;
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  
+}
+</style>
    
   
