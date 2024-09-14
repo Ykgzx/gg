5ce27776aedf63f525e7a6cefda6b28aad766a8a
9OAT <oathaha30@gmail.com>
2024-09-14 23:36:11 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -274,7 +274,7 @@ app.post('/api/login', async (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
